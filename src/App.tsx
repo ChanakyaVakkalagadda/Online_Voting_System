b9a0500f354a7439f4afbb4c7cb6f754333b1a69
ChanakyaVakkalagadda <chanakyavakkalagadda@gmail.com>
2025-11-27 23:05:04 +0530

    Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Online_Voting_System">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/results" element={<Results />} />
